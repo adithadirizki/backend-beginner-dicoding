899f87bb3fa6735595c48d4dd2fe775162a2b61c
Adit Hadi Rizki <adit.hadirizki@gmail.com>
2022-03-21 09:04:27 +0700

    postgres database implemented

--- src/handler.js
+++ src/handler.js
@@ -3,7 +3,7 @@ const ClientError = require("./exceptions/ClientError");
 class NotesHandler {
   /**
    *
-   * @param {import("./services/inMemory/NotesService")} service
+   * @param {import("./services/postgres/NotesService")} service
    * @param {import("./validator/notes/index")} validator
    */
   constructor(service, validator) {
@@ -24,13 +24,13 @@ class NotesHandler {
    * @param {import("@hapi/hapi").ResponseToolkit} h
    * @returns
    */
-  postNoteHandler(request, h) {
+  async postNoteHandler(request, h) {
     try {
       this._validator.validateNotePayload(request.payload);
 
       const { title = "untitled", body, tags } = request.payload;
 
-      const noteId = this._service.addNote({ title, body, tags });
+      const noteId = await this._service.addNote({ title, body, tags });
 
       const response = h.response({
         status: "success",
@@ -70,8 +70,8 @@ class NotesHandler {
    * @param {import("@hapi/hapi").ResponseToolkit} h
    * @returns
    */
-  getNotesHandler(request, h) {
-    const notes = this._service.getNotes();
+  async getNotesHandler(request, h) {
+    const notes = await this._service.getNotes();
     return {
       status: "success",
       data: {
@@ -87,10 +87,10 @@ class NotesHandler {
    * @param {import("@hapi/hapi").ResponseToolkit} h
    * @returns
    */
-  getNoteByIdHandler(request, h) {
+  async getNoteByIdHandler(request, h) {
     try {
       const { id } = request.params;
-      const note = this._service.getNoteById(id);
+      const note = await this._service.getNoteById(id);
 
       return {
         status: "success",
@@ -126,13 +126,13 @@ class NotesHandler {
    * @param {import("@hapi/hapi").ResponseToolkit} h
    * @returns
    */
-  putNoteByIdHandler(request, h) {
+  async putNoteByIdHandler(request, h) {
     try {
       this._validator.validateNotePayload(request.payload);
 
       const { id } = request.params;
 
-      this._service.editNoteById(id, request.payload);
+      await this._service.editNoteById(id, request.payload);
 
       return {
         status: "success",
@@ -166,10 +166,10 @@ class NotesHandler {
    * @param {import("@hapi/hapi").ResponseToolkit} h
    * @returns
    */
-  deleteNoteByIdHandler(request, h) {
+  async deleteNoteByIdHandler(request, h) {
     try {
       const { id } = request.params;
-      this._service.deleteNoteById(id);
+      await this._service.deleteNoteById(id);
 
       return {
         status: "success",
